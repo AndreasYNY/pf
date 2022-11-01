2d8269c64cb7b1fbb3eada22d28313be42ed416f
flora <flora@xilla>
2022-11-01 23:34:33 +0700

    update packages & change index looks

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from "next/head"
 import { useState } from "react"
+import { SimpleIcon } from "simple-icons"
 
 export default function Load() {
 
@@ -18,10 +19,18 @@ export default function Load() {
       <title>Andreas Yozef</title>
     </Head>
     
-    <div onLoad={loading()} className="bg-slate-800 h-screen grid place-items-center">
+    <div onLoad={loading()} className="absolute left-1 bottom-1">
       <h1 className={load + ` text-slate-50 text-4xl`}>Andreas <span className="font-bold ">Yozef</span></h1>
+      <p className={load + ` text-slate-50`}>I make website for living 🍙, Backend dev 🎒, <br />Linux enthusiast 🐧</p>
+      {/* <ul>
+        <li><a href="">test</a></li>
+      </ul> */}
     </div>
 
-    
+    <div className="absolute right-1 bottom-1">
+      <span className={load + ` text-slate-50`}>Made with ❤️ by <b><a href="https://github.com/andreasyny">andreasyny</a></b></span>
+    </div>
+
+    <div className="bg-slate-800 h-screen"></div>
   </>  
 }
